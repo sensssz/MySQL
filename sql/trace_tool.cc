a9530b79af46a3d8e55c75a8d6bcb390cd668137
sensssz <hjmsens@gmail.com>
2015-05-21 15:48:54 -0400

    Fix number of functions.

--- sql/trace_tool.cc
+++ sql/trace_tool.cc
@@ -138,7 +138,7 @@ TraceTool::TraceTool() : function_times()
 #ifdef MONITOR
   const int number_of_functions = NUMBER_OF_FUNCTIONS + 2;
 #else
-  const int number_of_functions = NUMBER_OF_FUNCTIONS + 4;
+  const int number_of_functions = NUMBER_OF_FUNCTIONS + 1;
 #endif
   for (int index = 0; index < number_of_functions; index++)
   {
