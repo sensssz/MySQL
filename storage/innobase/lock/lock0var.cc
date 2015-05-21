05ea018afcbfe29ccf9417420332315399d6635c
sensssz <hjmsens@gmail.com>
2015-05-21 19:53:28 -0400

    Commit for switching branch.

--- storage/innobase/lock/lock0var.cc
+++ storage/innobase/lock/lock0var.cc
@@ -134,31 +134,118 @@ compare(lock_t *lock1, lock_t *lock2)
 
 static
 void
-h(parameters &para, ulint size, double *betas, double *gammas, double *gamma_stars)
+g_starter(ulint size, double *betas, double *gammas, double *beta_stars, bool *solution)
 {
+  parameters para;
+  para.k = 3;
+  para.heu = 0;
   
+  unordered_map<parameters, double> cache;
+  h_recursive(para, size, betas, gammas, beta_stars, cache, solution);
 }
 
 static
 void
-h_recursive(parameters &para, ulint size,
-            double *betas, double *gammas, double *gamma_stars,
-            unordered_map<parameters, double> &cache)
+g_recursive(parameters &para, ulint size,
+            double *betas, double *gammas, double *beta_stars,
+            unordered_map<parameters, double> &cache,
+            bool *solution)
 {
-  if (para.k == size + 1)
+  ulint k = para.k;
+  double beta = para.heu;
+  // g(1) = 0
+  if (k == 0)
   {
     return 0;
   }
   unordered_map<parameters, double>::iterator iterator = cache.find(para);
   if (iterator != cache.end())
   {
+    // Already calculated the value for this pair of parameters
     return *iterator;
   }
   
-  parameters new_para;
-  new_para.k = para.k + 1;
-  new_para.heu = para.heu;
-  double put_before = h_recursive(new_para, size, betas, gammas, gamma_stars, cache);
+  parameters new_para_before;
+  new_para_before.k = k - 1;
+  new_para_before.heu = beta;
+  double recursive_before = h_recursive(new_para, size, betas, gammas, gamma_stars, cache);
+  double put_before = recursive_befor + gammas[k] * beta;
+  
+  parameters new_para_after;
+  new_para_after.k = k;
+  new_para_after.heu += gammas[k];
+  double recursive_after = h_recursive(new_para, size, betas, gammas, gamma_stars, cache);
+  double put_after = recursive_after + gammas[k] * (beta_stars[k] - beta);
+  
+  cache[new_para_before] = recursive_before;
+  cache[new_para_after] = recursive_after;
+  
+  if (put_before > put_after)
+  {
+    solution[k] = 0;
+  }
+  else
+  {
+    solution[k] = 1;
+  }
+}
+
+static
+void
+h_starter(ulint size, double *betas, double *gammas, double *gamma_stars, bool *solution)
+{
+  parameters para;
+  para.k = 3;
+  para.heu = 0;
+  
+  unordered_map<parameters, double> cache;
+  h_recursive(para, size, betas, gammas, gamma_stars, cache, solution);
+}
+
+static
+void
+h_recursive(parameters &para, ulint size,
+            double *betas, double *gammas, double *gamma_stars,
+            unordered_map<parameters, double> &cache,
+            bool *solution)
+{
+  ulint k = para.k;
+  double gamma = para.heu;
+  // h(n + 1) = 0
+  if (k == size + 1)
+  {
+    return 0;
+  }
+  unordered_map<parameters, double>::iterator iterator = cache.find(para);
+  if (iterator != cache.end())
+  {
+    // Already calculated the value for this pair of parameters
+    return *iterator;
+  }
+  
+  parameters new_para_before;
+  new_para_before.k = k + 1;
+  new_para_before.heu = gamma;
+  double recursive_before = h_recursive(new_para, size, betas, gammas, gamma_stars, cache);
+  double put_before = recursive_befor + betas[k] * gamma;
+  
+  parameters new_para_after;
+  new_para_after.k = k;
+  new_para_after.heu += gammas[k];
+  double recursive_after = h_recursive(new_para, size, betas, gammas, gamma_stars, cache);
+  double put_after = recursive_after + betas[k] * (gamma_stars[k - 1] - gamma);
+  
+  cache[new_para_before] = recursive_before;
+  cache[new_para_after] = recursive_after;
+  
+  if (put_before > put_after)
+  {
+    solution[k] = 0;
+  }
+  else
+  {
+    solution[k] = 1;
+  }
 }
 
 /*************************************************************//**
@@ -187,7 +274,7 @@ CTV_schedule(vector<lock_t *> &locks) /*!< candidate locks */
     lock->process_time = estimate(time_so_far);
   }
   sort(locks.begin(), locks.end(), compare);
-  final_schedule[0] = locks.back();
+  // Calculate betas and gammas
   for (ulint index = 0; index < size; ++index)
   {
     ulint sum = 0;
@@ -199,19 +286,65 @@ CTV_schedule(vector<lock_t *> &locks) /*!< candidate locks */
     as[index] = process_time + sum / 2.0;
     betas[index] = (size - index) * process_time + 2 * as[index];
     gammas[index] = (index + 1) * process_time - 2 * as[index];
-    if (index == 0)
-    {
-      gamma_stars[index] = gammas[index];
-    }
-    else
+  }
+  
+  beta_stars[size - 2] = betas[size - 1];
+  gamma_stars[1] = gamma_stars[1];
+  // Calculate beta stars and gamma stars
+  for (int index = 1; index < size - 1; ++index)
+  {
+    if (index > 1)
     {
       gamma_stars[index] = gamma_stars[index - 1] + gammas[index];
     }
+    else if (index < size - 2)
+    {
+      beta_stars[size - 2 - index] = beta_stars[size - 2 - index + 1] + beta[size - 2 - index + 1];
+    }
   }
-  gamma_star = gamma_stars[size - 1];
+  // The last one is always at the first place
+  quadratic_solution[size - 1] = 0;
+  
+  beta_stars = beta_stars[1];
+  gamma_star = gamma_stars[size - 2];
+  
+  if (beta_star < gamma_star)
+  {
+    g_starter(size, betas, gamms, beta_stars, quadratic_solution);
+  }
+  else
+  {
+    h_starter(size, betas, gamms, gamma_stars, quadratic_solution);
+  }
+  
+  int schedule_index = 1;
+  final_schedule[0] = locks.back();
+  for (int solution_index = size - 1; solution_index > 0; --solution_index)
+  {
+    if (quadratic_solution[solution_index] == 0)
+    {
+      // Should be moved before the shortest job
+      final_schedule[schedule_index++] = locks[solution_index];
+    }
+  }
+  final_schedule[schedule_index++] = locks[0];
+  for (int solution_index = 1; solution_index < size; ++solution_index)
+  {
+    // Should be moved after the shortest job
+    if (quadratic_solution[solution_index] == 1)
+    {
+      final_schedule[schedule_index++] = locks[solution_index];
+    }
+  }
+  
+  for (int index = 0; index <= size; ++index)
+  {
+    locks[index] = final_schedule[index];
+  }
   
   delete[] as;
   delete[] betas;
+  delete[] beta_stars;
   delete[] gammas;
   delete[] gamma_stars;
   delete[] quadratic_solution;
