aeee4445f98fc044b6fa48499e2cd235ed8affb6
sensssz <hjmsens@gmail.com>
2015-05-25 17:38:00 -0400

    Bug fix.

--- sql/trace_tool.cc
+++ sql/trace_tool.cc
@@ -6,6 +6,8 @@
 #include <time.h>
 #include <cstring>
 #include <sstream>
+#include <cstdlib>
+#include <cassert>
 
 #ifndef _GNU_SOURCE
 #define CLOCK_REALTIME 1
@@ -17,6 +19,12 @@ void clock_gettime(int, timespec *)
 #define NUMBER_OF_FUNCTIONS 0
 #define LATENCY
 
+#define NEW_ORDER_MARKER "SELECT C_DISCOUNT, C_LAST, C_CREDIT, W_TAX  FROM CUSTOMER, WAREHOUSE WHERE"
+#define PAYMENT_MARKER "UPDATE WAREHOUSE SET W_YTD = W_YTD"
+#define ORDER_STATUS_MARKER "SELECT C_FIRST, C_MIDDLE"
+#define DELIVERY_MARKER "SELECT NO_O_ID FROM NEW_ORDER WHERE NO_D_ID ="
+#define STOCK_LEVEL_MARKER "SELECT D_NEXT_O_ID FROM DISTRICT WHERE D_W_ID ="
+
 #define EQUAL(struct1, struct2, field) (struct1->field == struct2->field)
 
 using std::endl;
@@ -25,38 +33,37 @@ using std::vector;
 using std::stringstream;
 using std::sort;
 
-long transaction_id = 0;
+ulint transaction_id = 0;
 
 TraceTool *TraceTool::instance = NULL;
 pthread_mutex_t TraceTool::instance_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_rwlock_t TraceTool::data_lock = PTHREAD_RWLOCK_INITIALIZER;
-__thread long TraceTool::current_transaction_id = 0;
-__thread timespec TraceTool::last_query;
+__thread ulint TraceTool::current_transaction_id = 0;
 
-timespec TraceTool::start_time = {0, 0};
 timespec TraceTool::global_last_query;
 pthread_mutex_t TraceTool::last_query_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t TraceTool::record_lock_mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t TraceTool::average_mutex = PTHREAD_MUTEX_INITIALIZER;
-ulint TraceTool::commited_trx = 0;
-double TraceTool::average_latency = 0;
-double TraceTool::average_work_time = 0;
-ulint TraceTool::num_deadlocks = 0;
 
 __thread int TraceTool::path_count = 0;
 __thread bool TraceTool::is_commit = false;
-__thread bool TraceTool::commit_successful = false;
+__thread bool TraceTool::commit_successful = true;
 __thread bool TraceTool::new_transaction = true;
+__thread timespec TraceTool::trans_start;
+__thread transaction_type TraceTool::type = NONE;
 
 
+static const size_t NEW_ORDER_LENGTH = strlen(NEW_ORDER_MARKER);
+static const size_t PAYMENT_LENGTH = strlen(PAYMENT_MARKER);
+static const size_t ORDER_STATUS_LENGTH = strlen(ORDER_STATUS_MARKER);
+static const size_t DELIVERY_LENGTH = strlen(DELIVERY_MARKER);
+static const size_t STOCK_LEVEL_LENGTH = strlen(STOCK_LEVEL_MARKER);
+
+/* Define MONITOR if needs to trace running time of functions. */
 #ifdef MONITOR
 static __thread timespec function_start;
 static __thread timespec function_end;
 static __thread timespec call_start;
 static __thread timespec call_end;
 #endif
-
-static int log_index = 0;
 
 void TRACE_FUNCTION_START()
 {
@@ -102,16 +109,21 @@ bool TRACE_END(int index)
   return false;
 }
 
+/********************************************************************//**
+Get the current TraceTool instance. */
 TraceTool *TraceTool::get_instance()
 {
   if (instance == NULL)
   {
     pthread_mutex_lock(&instance_mutex);
+    /* Check instance again after entering the ciritical section
+       to prevent double initilization. */
     if (instance == NULL)
     {
       instance = new TraceTool;
 #ifdef LATENCY
-      start_time = get_time();
+      /* Create a background thread for dumping function running time
+         and latency data. */
       pthread_t write_thread;
       pthread_create(&write_thread, NULL, check_write_log, NULL);
 #endif
@@ -123,7 +135,8 @@ TraceTool *TraceTool::get_instance()
 
 TraceTool::TraceTool() : function_times()
 {
-  log_file.open("trace.log");
+  /* Open the log file in append mode so that it won't be overwritten */
+  log_file.open("logs/trace.log", std::ofstream::out | std::ofstream::app);
 #ifdef MONITOR
   const int number_of_functions = NUMBER_OF_FUNCTIONS + 2;
 #else
@@ -131,12 +144,15 @@ TraceTool::TraceTool() : function_times()
 #endif
   for (int index = 0; index < number_of_functions; index++)
   {
-    vector<long> function_time;
+    vector<ulint> function_time;
     function_time.reserve(500000);
     function_time.push_back(0);
     function_times.push_back(function_time);
   }
+  transaction_start_times.reserve(500000);
   transaction_start_times.push_back(0);
+  transaction_types.reserve(50000);
+  transaction_types.push_back(NONE);
 }
 
 bool TraceTool::should_monitor()
@@ -146,23 +162,31 @@ bool TraceTool::should_monitor()
 
 void *TraceTool::check_write_log(void *arg)
 {
+  /* Runs in an infinite loop and for every 5 seconds,
+     check if there's any query comes in. If not, then
+     dump data to log files. */
   while (true)
   {
     sleep(5);
     timespec now = get_time();
     if (now.tv_sec - global_last_query.tv_sec >= 5 && transaction_id > 0)
     {
-      std::ifstream src("trace.log", std::ios::binary);
-      std::ofstream dst("trace.bak", std::ios::binary);
+      /* Create a back up of the debug log file in case it's overwritten. */
+      std::ifstream src("logs/trace.log", std::ios::binary);
+      std::ofstream dst("logs/trace.bak", std::ios::binary);
       dst << src.rdbuf();
+      src.close();
+      dst.close();
       
+      /* Create a new TraceTool instnance. */
       TraceTool *old_instace = instance;
       instance = new TraceTool;
-      transaction_id = 0;
-      average_latency = 0;
-      average_work_time = 0;
-      commited_trx = 0;
       
+      /* Reset the global transaction ID. */
+      transaction_id = 0;
+      
+      /* Dump data in the old instance to log files and
+         reclaim memory. */
       old_instace->write_log();
       delete old_instace;
     }
@@ -177,118 +201,119 @@ timespec TraceTool::get_time()
   return now;
 }
 
-bool TraceTool::time_before(timespec &time1, timespec &time2)
-{
-  return (time1.tv_sec < time2.tv_sec) ||
-      (time1.tv_sec == time2.tv_sec && time1.tv_nsec < time2.tv_nsec);
-}
-
-long TraceTool::difftime(timespec &start, timespec &end)
+ulint TraceTool::difftime(timespec start, timespec end)
 {
   return (end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec);
 }
 
-long now_micro()
+ulint TraceTool::now_micro()
 {
   timespec now;
   clock_gettime(CLOCK_REALTIME, &now);
   return now.tv_sec * 1000000 + now.tv_nsec / 1000;
 }
 
-bool lock_equal(lock_info *lock1, lock_info *lock2)
-{
-  return EQUAL(lock1, lock2, space_id) && EQUAL(lock1, lock2, page_no) &&
-  EQUAL(lock1, lock2, heap_no) && EQUAL(lock1, lock2, type_mode);
-}
-
-void TraceTool::start_waiting(lock_info *lock_info, lock_request *request)
-{
-  pthread_mutex_lock(&record_lock_mutex);
-  record_lock *lock_waiting_on = record_lock_map[*lock_info];
-  if (lock_waiting_on == NULL)
-  {
-    lock_waiting_on = new record_lock;
-    lock_waiting_on->info.space_id = lock_info->space_id;
-    lock_waiting_on->info.page_no = lock_info->page_no;
-    lock_waiting_on->info.heap_no = lock_info->heap_no;
-    lock_waiting_on->info.type_mode = lock_info->type_mode;
-    record_lock_map[*lock_info] = lock_waiting_on;
-  }
-  pthread_mutex_unlock(&record_lock_mutex);
-  request->lock = lock_waiting_on;
-  request->start_waiting_time = get_time();
-}
-
-void TraceTool::end_waiting(lock_request *request)
-{
-  if (request != NULL && request->lock_object != NULL)
-  {
-    timespec now =  get_time();
-    long waiting_time = difftime(request->start_waiting_time, now);
-    request->lock->waiting_times.push_back(waiting_time);
-  }
-}
-
+/********************************************************************//**
+Start a new query. This may also start a new transaction. */
 void TraceTool::start_new_query()
 {
   is_commit = false;
+  /* This happens when a log write happens, which marks the end of a phase. */
   if (current_transaction_id > transaction_id)
   {
     current_transaction_id = 0;
+    new_transaction = true;
+    commit_successful = true;
   }
 #ifdef LATENCY
+  /* Start a new transaction. Note that we don't reset the value of new_transaction here.
+     We do it in set_query after looking at the first query of a transaction. */
   if (new_transaction)
   {
-    new_transaction = false;
+    trans_start = get_time();
     commit_successful = true;
+    /* Use a write lock here because we are appending content to the vector. */
     pthread_rwlock_wrlock(&data_lock);
-    ulint latency = function_times.back()[current_transaction_id];
-    ++commited_trx;
-    average_latency = (average_latency * (commited_trx - 1) + latency) / commited_trx;
     current_transaction_id = transaction_id++;
     transaction_start_times[current_transaction_id] = now_micro();
-    
-    for (vector<vector<long> >::iterator iterator = function_times.begin(); iterator != function_times.end(); ++iterator)
+    for (vector<vector<ulint> >::iterator iterator = function_times.begin();
+         iterator != function_times.end();
+         ++iterator)
     {
       iterator->push_back(0);
     }
     transaction_start_times.push_back(0);
-    
+    transaction_types.push_back(NONE);
     pthread_rwlock_unlock(&data_lock);
   }
-  clock_gettime(CLOCK_REALTIME, &last_query);
   pthread_mutex_lock(&last_query_mutex);
   clock_gettime(CLOCK_REALTIME, &global_last_query);
   pthread_mutex_unlock(&last_query_mutex);
 #endif
 }
 
+void TraceTool::set_query(const char *new_query)
+{
+  // Look at the first query of a transaction
+  if (new_transaction)
+  {
+    if (strncmp(new_query, NEW_ORDER_MARKER, NEW_ORDER_LENGTH) == 0)
+    {
+      type = NEW_ORDER;
+    }
+    else if (strncmp(new_query, PAYMENT_MARKER, PAYMENT_LENGTH) == 0)
+    {
+      type = PAYMENT;
+    }
+    else if (strncmp(new_query, ORDER_STATUS_MARKER, ORDER_STATUS_LENGTH) == 0)
+    {
+      type = ORDER_STATUS;
+    }
+    else if (strncmp(new_query, DELIVERY_MARKER, DELIVERY_LENGTH) == 0)
+    {
+      type = DELIVERY;
+    }
+    else if (strncmp(new_query, STOCK_LEVEL_MARKER, STOCK_LEVEL_LENGTH) == 0)
+    {
+      type = STOCK_LEVEL;
+    }
+    
+    transaction_types[current_transaction_id] = type;
+    /* Reset the value of new_transaction. */
+    new_transaction = false;
+  }
+}
+
 void TraceTool::end_query()
 {
 #ifdef LATENCY
-  if (current_transaction_id > transaction_id)
+  if (is_commit)
   {
-    current_transaction_id = 0;
+    end_transaction();
   }
-  timespec now = get_time();
-  long latency = difftime(last_query, now);
-  pthread_rwlock_rdlock(&data_lock);
-  function_times.back()[current_transaction_id] += latency;
-  pthread_rwlock_unlock(&data_lock);
 #endif
 }
 
 void TraceTool::end_transaction()
 {
+  new_transaction = true;
+  type = NONE;
 #ifdef LATENCY
   if (commit_successful)
   {
-    new_transaction = true;
+    timespec now = get_time();
+    long latency = difftime(trans_start, now);
+    pthread_rwlock_rdlock(&data_lock);
+    function_times.back()[current_transaction_id] = latency;
+    pthread_rwlock_unlock(&data_lock);
   }
   else
   {
+    /* Commit fails, we set its latency to 0 and ignore it. */
     pthread_rwlock_rdlock(&data_lock);
+    // Reuse the last transaction
     function_times.back()[current_transaction_id] = 0;
+    transaction_start_times[current_transaction_id] = 0;
     pthread_rwlock_unlock(&data_lock);
   }
 #endif
@@ -300,63 +325,120 @@ void TraceTool::add_record(int function_index, long duration)
   {
     current_transaction_id = 0;
   }
-  // We use a read lock here becuase only the
-  // current thread can change the corresponing record
   pthread_rwlock_rdlock(&data_lock);
   function_times[function_index][current_transaction_id] += duration;
   pthread_rwlock_unlock(&data_lock);
 }
 
-bool compare_record_lock(record_lock *lock1, record_lock *lock2)
+void TraceTool::write_latency()
 {
-  return lock1->waiting_times.size() > lock2->waiting_times.size();
-}
-
-void TraceTool::write_log()
-{
-  ofstream log;
+  ofstream overall_log;
+  ofstream new_order_log;
+  ofstream payment_log;
+  ofstream order_status_log;
+  ofstream delivery_log;
+  ofstream stock_level_log;
+  
   stringstream sstream;
-  sstream << "trace" << log_index++;
-  log.open(sstream.str().c_str());
-  ulint function_index = 0;
-  double num_trx = 0;
+  sstream << "logs/tpcc";
+  overall_log.open(sstream.str().c_str());
+  
+  sstream.str("");
+  sstream << "logs/new_order";
+  new_order_log.open(sstream.str().c_str());
+  
+  sstream.str("");
+  sstream << "logs/payment";
+  payment_log.open(sstream.str().c_str());
+  
+  sstream.str("");
+  sstream << "logs/order_status";
+  order_status_log.open(sstream.str().c_str());
+  
+  sstream.str("");
+  sstream << "logs/delivery";
+  delivery_log.open(sstream.str().c_str());
+  
+  sstream.str("");
+  sstream << "logs/stock_level";
+  stock_level_log.open(sstream.str().c_str());
+  
+  int function_index = 0;
   pthread_rwlock_wrlock(&data_lock);
-  num_trx = transaction_start_times.size();
   for (unsigned long index = 0; index < transaction_start_times.size(); ++index)
   {
-    log << transaction_start_times[index] << endl;
+    ulint start_time = transaction_start_times[index];
+    if (start_time > 0)
+    {
+      overall_log << start_time << endl;
+      switch (transaction_types[index])
+      {
+        case NEW_ORDER:
+          new_order_log << start_time << endl;
+          break;
+        case PAYMENT:
+          payment_log << start_time << endl;
+          break;
+        case ORDER_STATUS:
+          order_status_log << start_time << endl;
+          break;
+        case DELIVERY:
+          delivery_log << start_time << endl;
+          break;
+        case STOCK_LEVEL:
+          stock_level_log << start_time << endl;
+          break;
+        default:
+          break;
+      }
+    }
   }
   
-  for (vector<vector<long> >::iterator iterator = function_times.begin(); iterator != function_times.end(); ++iterator)
+  for (vector<vector<ulint> >::iterator iterator = function_times.begin(); iterator != function_times.end(); ++iterator)
   {
     long number_of_transactions = iterator->size();
     for (long index = 0; index < number_of_transactions; ++index)
     {
-      log << function_index << ',' << (*iterator)[index] << endl;
+      if (function_times.back()[index] > 0)
+      {
+        ulint latency = (*iterator)[index];
+        overall_log << function_index << ',' << latency << endl;
+        switch (transaction_types[index])
+        {
+          case NEW_ORDER:
+            new_order_log << function_index << ',' << latency << endl;
+            break;
+          case PAYMENT:
+            payment_log << function_index << ',' << latency << endl;
+            break;
+          case ORDER_STATUS:
+            order_status_log << function_index << ',' << latency << endl;
+            break;
+          case DELIVERY:
+            delivery_log << function_index << ',' << latency << endl;
+            break;
+          case STOCK_LEVEL:
+            stock_level_log << function_index << ',' << latency << endl;
+            break;
+          default:
+            break;
+        }
+      }
     }
     function_index++;
     iterator->clear();
   }
   function_times.clear();
   pthread_rwlock_unlock(&data_lock);
-  log.close();
-  
-  ofstream lock_waiting_log("lock");
-  pthread_mutex_lock(&record_lock_mutex);
-  lock_waiting_log << "Rate of deadlocks: " << num_deadlocks / num_trx << endl;
-  lock_waiting_log << "Number of locks: " << record_lock_map.size() << endl;
-  for (auto lock_map : record_lock_map)
-  {
-    record_lock *lock = lock_map.second;
-    lock_waiting_log << "Lock: " << lock->info.space_id << "," << lock->info.page_no << ","
-    << lock->info.heap_no << "," << lock->info.type_mode << "," << lock->waiting_times.size() << endl;
-    for (auto time : lock->waiting_times)
-    {
-      lock_waiting_log << time << endl;
-    }
-    delete lock;
-  }
-  pthread_mutex_unlock(&record_lock_mutex);
-  record_lock_map.clear();
-  lock_waiting_log.close();
+  overall_log.close();
+  new_order_log.close();
+  payment_log.close();
+  order_status_log.close();
+  delivery_log.close();
+  stock_level_log.close();
+}
+
+void TraceTool::write_log()
+{
+  write_latency();
 }
--- sql/trace_tool.h
+++ sql/trace_tool.h
@@ -1,89 +1,156 @@
 #ifndef MY_TRACE_TOOL_H
 #define MY_TRACE_TOOL_H
 
+#include "../storage/innobase/include/os0sync.h"
 #include "../storage/innobase/include/lock0types.h"
 #include <fstream>
-#include <deque>
+#include <list>
 #include <vector>
 #include <pthread.h>
 #include <time.h>
 #include <unordered_map>
+#include <cstdlib>
+#include <string>
 
+/** This macro is used for tracing the running time of
+    a function call which appears inside an if statement*/
 #define TRACE_S_E(function_call, index) (TRACE_START()|(function_call)|TRACE_END(index))
 
 typedef unsigned long int ulint;
+typedef unsigned int uint;
 
 using std::ofstream;
-using std::deque;
+using std::list;
 using std::vector;
 using std::endl;
 using std::unordered_map;
+using std::string;
 
-extern long transaction_id;
+/** The global transaction id counter */
+extern ulint transaction_id;
+
+/********************************************************************//**
+To break down the variance of a function, we need to trace the running
+time of a function and the functions it calls. */
+
+/********************************************************************//**
+This function marks the start of a function call */
 void TRACE_FUNCTION_START();
+
+/********************************************************************//**
+This function marks the end of a function call */
 void TRACE_FUNCTION_END();
+
+/********************************************************************//**
+This function marks the start of a child function call. */
 bool TRACE_START();
-bool TRACE_END(int index);
+
+/********************************************************************//**
+This function marks the end of a child function call. */
+bool TRACE_END(
+  int index);   /*!< Index of the child function call. */
+
+/********************************************************************//**
+Transaction types in TPCC workload. */
+enum transaction_type
+{
+    NEW_ORDER, PAYMENT, ORDER_STATUS, DELIVERY, STOCK_LEVEL, NONE
+};
+typedef enum transaction_type transaction_type;
 
 class TraceTool
 {
 private:
-    static TraceTool *instance;
-    static pthread_mutex_t instance_mutex;
+    static TraceTool *instance;             /*!< Instance for the Singleton pattern. */
+    static pthread_mutex_t instance_mutex;  /*!< Mutex for protecting instance. */
     
-    static __thread long current_transaction_id;
-    static __thread timespec last_query;
+    static timespec global_last_query;      /*!< Time when MySQL receives the most recent query. */
+    static pthread_mutex_t last_query_mutex;/*!< Mutex for protecting global_last_query */
     
-    static timespec start_time;
-    static timespec global_last_query;
-    static pthread_mutex_t last_query_mutex;
-    static pthread_mutex_t record_lock_mutex;
-    static pthread_mutex_t average_mutex;
-    static ulint commited_trx;
+    static __thread timespec function_start;/*!< Time for the start of a function call. */
+    static __thread timespec function_end;  /*!< Time for the end of a function call. */
+    static __thread timespec call_start;    /*!< Time for the start of a child function call. */
+    static __thread timespec call_end;      /*!< Time for the end of a child function call. */
+    static __thread bool new_transaction;   /*!< True if we need to start a new transaction. */
+    static __thread timespec trans_start;   /*!< Start time of the current transaction. */
     
-    static __thread timespec function_start;
-    static __thread timespec function_end;
-    static __thread timespec call_start;
-    static __thread timespec call_end;
-    static __thread bool new_transaction;
+    ofstream log_file;                      /*!< An log file for outputing debug messages. */
     
-    ofstream log_file;
-    static pthread_rwlock_t data_lock;
-    vector<vector<long> > function_times;
-    vector<long> transaction_start_times;
-    unordered_map<lock_info, record_lock *> record_lock_map;
+    static pthread_rwlock_t data_lock;      /*!< A read-write lock for protecting function_times. */
+    vector<vector<ulint> > function_times;  /*!< Stores the running time of the child functions
+                                                 and also transaction latency (the last one). */
+    vector<ulint> transaction_start_times;  /*!< Stores the start time of transactions. */
+    vector<transaction_type> transaction_types;/*!< Stores the transaction types of transactions. */
     
     TraceTool();
     TraceTool(TraceTool const&){};
 public:
-    static double average_latency;
-    static double average_work_time;
-    static ulint num_deadlocks;
-    static __thread int path_count;
-    static __thread bool is_commit;
-    static __thread bool commit_successful;
+    static __thread ulint current_transaction_id;   /*!< Each thread can execute only one transaction at
+                                                         a time. This is the ID of the current transactions. */
     
-    static long get_current_transaction_id()
-    {
-        return current_transaction_id;
-    }
+    static __thread int path_count;         /*!< Number of node in the function call path. Used for
+                                                 tracing running time of functions. */
+    
+    static __thread bool is_commit;         /*!< True if the current transactions commits. */
+    static __thread bool query_is_commit;   /*!< True if the current query is a commit query (Note that
+                                                 these two doesn't have to be true at the same time). */
+    static __thread bool commit_successful; /*!< True if the current transaction successfully commits. */
+    static __thread transaction_type type;  /*!< Type of the current transaction. */
+    
+    /********************************************************************//**
+    The Singleton pattern. Used for getting the instance of this class. */
     static TraceTool *get_instance();
-    static bool should_monitor();
-    static bool time_before(timespec &time1, timespec &time2);
-    static long difftime(timespec &start, timespec &end);
-    static void *check_write_log(void *);
-    static timespec get_time();
     
-    void start_waiting(lock_info *lock_info, lock_request *request);
-    void end_waiting(lock_request *request);
+    /********************************************************************//**
+    Check if we should trace the running time of function calls. */
+    static bool should_monitor();
+    
+    /********************************************************************//**
+    Calcualte time interval in nanoseconds. */
+    static ulint difftime(timespec start, timespec end);
+    
+    /********************************************************************//**
+    Periododically checks if any query comes in in the last 5 second.
+    If no then dump all logs to disk. */
+    static void *check_write_log(void *);
+    
+    /********************************************************************//**
+    Get the current time in nanosecond. */
+    static timespec get_time();
+    /********************************************************************//**
+    Get the current tiem in microsecond. */
+    static ulint now_micro();
+    
+    /********************************************************************//**
+    Retruns the log file for outputing debug information. */
     ofstream &get_log()
     {
         return log_file;
     }
+    
+    /********************************************************************//**
+    Start a new query. This may also start a new transaction. */
     void start_new_query();
+    /********************************************************************//**
+    End a new query. This may also end the current transaction. */
     void end_query();
+    /********************************************************************//**
+    End the current transaction. */
     void end_transaction();
+    
+    /********************************************************************//**
+    Analysis the current query to find out the transaction type. */
+    void set_query(const char *query);
+    
+    /********************************************************************//**
+    Dump data about function running time and latency to log file. */
+    void write_latency();
+    /********************************************************************//**
+    Write necessary data to log files. */
     void write_log();
+    
+    /********************************************************************//**
+    Record running time of a function. */
     void add_record(int function_index, long duration);
 };
 
