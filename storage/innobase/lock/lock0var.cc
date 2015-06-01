aa296e5aaebee6d161fd21bb73838620146689d6
sensssz <hjmsens@gmail.com>
2015-05-31 21:33:19 -0400

    Finish the code.

--- storage/innobase/lock/lock0var.cc
+++ storage/innobase/lock/lock0var.cc
@@ -22,26 +22,6 @@ using std::sort;
 using std::find;
 using std::string;
 
-static ulint *tpcc_work_wait = NULL;
-static ulint *tpcc_estimated = NULL;
-static ulint *new_order_work_wait = NULL;
-static ulint *new_order_estimated = NULL;
-static ulint *payment_work_wait = NULL;
-static ulint *payment_estimated = NULL;
-static ulint *order_status_work_wait = NULL;
-static ulint *order_status_estimated = NULL;
-static ulint *delivery_work_wait = NULL;
-static ulint *delivery_estimated = NULL;
-static ulint *stock_level_work_wait = NULL;
-static ulint *stock_level_estimated = NULL;
-
-static ulint tpcc_length = 0;
-static ulint new_order_length = 0;
-static ulint payment_length = 0;
-static ulint order_status_length = 0;
-static ulint delivery_length = 0;
-static ulint stock_level_length = 0;
-
 static
 string
 lock_get_mode(
@@ -76,18 +56,6 @@ UNIV_INTERN
 void
 indi_init()
 {
-  read_isotonic("isotonic_tpcc", tpcc_work_wait,
-                tpcc_estimated, tpcc_length);
-  read_isotonic("isotonic_new_order", new_order_work_wait,
-                new_order_estimated, new_order_length);
-  read_isotonic("isotonic_payment", payment_work_wait,
-                payment_estimated, payment_length);
-  read_isotonic("isotonic_order_status", order_status_work_wait,
-                order_status_estimated, order_status_length);
-  read_isotonic("isotonic_delivery", delivery_work_wait,
-                delivery_estimated, delivery_length);
-  read_isotonic("isotonic_stock_level", stock_level_work_wait,
-                stock_level_estimated, stock_level_length);
 }
 
 /*************************************************************//**
@@ -141,102 +109,72 @@ binary_search(
   return length - 1;
 }
 
+static
+ulint
+tpcc_estimate(ulint num_locks, ulint work, ulint wait)
+{
+  return 5488089.615703 * num_locks + 1.942222 * work + 0.766192 * wait + 298666199.000513;
+}
+
+static
+ulint
+new_order_estimate(ulint num_locks, ulint work, ulint wait)
+{
+  return 9849050.582090 * num_locks + 2.393067 * work + 0.788607 * wait + 293570030.003463;
+}
+
+static
+ulint
+payment_estimate(ulint num_locks, ulint work, ulint wait)
+{
+  return 148662428.974091 * num_locks + 1.548239 * work + 0.736832 * wait + 0.000000;
+}
+
+static
+ulint
+delivery_estimate(ulint num_locks, ulint work, ulint wait)
+{
+  return -13306266.992916 * num_locks + 2.613634 * work + 0.652131 * wait + 907693255.087883;
+}
+
+static
+ulint
+stock_level_estimate(ulint num_locks, ulint work, ulint wait)
+{
+  return -444879.387608 * num_locks + 0.866669 * work + 0.944012 * wait + 193479572.897411;
+}
+
 /*************************************************************//**
 Estimate remaining time given total time so far. */
 UNIV_INTERN
 ulint
 estimate(
-  ulint time_so_far,
-  transaction_type type)
+         ulint num_locks,
+         ulint work,
+         ulint wait,
+         transaction_type type)
 {
-  ulint *so_far = NULL;
-  ulint *estimate = NULL;
-  ulint length = 0;
-  
   switch (type) {
     case NONE:
-      so_far = tpcc_work_wait;
-      estimate = tpcc_estimated;
-      length = tpcc_length;
+      return tpcc_estimate(num_locks, work, wait);
     case NEW_ORDER:
-      so_far = new_order_work_wait;
-      estimate = new_order_estimated;
-      length = new_order_length;
+      return new_order_estimate(num_locks, work, wait);
       break;
     case PAYMENT:
-      so_far = payment_work_wait;
-      estimate = payment_estimated;
-      length = payment_length;
+      return payment_estimate(num_locks, work, wait);
       break;
     case ORDER_STATUS:
-      so_far = order_status_work_wait;
-      estimate = order_status_estimated;
-      length = order_status_length;
+      return tpcc_estimate(num_locks, work, wait);
       break;
     case DELIVERY:
-      so_far = delivery_work_wait;
-      estimate = delivery_estimated;
-      length = delivery_length;
+      return delivery_estimate(num_locks, work, wait);
       break;
     case STOCK_LEVEL:
-      so_far = stock_level_work_wait;
-      estimate = stock_level_estimated;
-      length = stock_level_length;
+      return stock_level_estimate(num_locks, work, wait);
       break;
     default:
+      return tpcc_estimate(num_locks, work, wait);
       break;
-  }
-  
-  ulint y_index = binary_search(so_far, length, time_so_far);
-  if (y_index == length - 1)
-  {
-    y_index = length - 2;
-  }
-  ulint result = estimate[y_index + 1];
-  return result - time_so_far;
-}
-
-/*************************************************************//**
-Swap two elements in a vector. */
-static
-void
-swap(
-  vector<lock_t *> &locks,
-  ulint index1,
-  ulint index2)
-{
-  if (index1 != index2)
-  {
-    lock_t *temp = locks[index1];
-    locks[index1] = locks[index2];
-    locks[index2] = temp;
-  }
-}
-
-/*************************************************************//**
-Generate all possible permutations of the given vector. */
-static
-void
-permutate(
-  vector<lock_t *> &locks,
-  ulint start,
-  ulint end,
-  vector<vector<lock_t *> > &permutations)
-{
-  if (start == end)
-  {
-    vector<lock_t *> permutation(locks);
-    permutations.push_back(permutation);
-    return;
-  }
-  
-  for (ulint index = start; index <= end; ++index)
-  {
-    /* Swap these two elements. */
-    swap(locks, start, index);
-    permutate(locks, start + 1, end, permutations);
-    /* Swap them back. */
-    swap(locks, start, index);
   }
 }
 
@@ -297,93 +235,6 @@ cumsum(
     rolling_sum.push_back(lock->time_so_far + lock->process_time + sum_of_previous_process_time);
   }
 }
-
-/*************************************************************//**
-Find the lock that gives minimum variance of estimated latency. */
-UNIV_INTERN
-lock_t *
-CTV_schedule(vector<lock_t *> &locks) /*!< candidate locks */
-{
-  if (locks.size() == 0)
-  {
-    return NULL;
-  }
-  else if (locks.size() == 1)
-  {
-    return locks[0];
-  }
-  
-  int random = rand() % 100;
-  bool do_log = random < 42;
-  
-  ofstream &log_file = TraceTool::get_instance()->get_log();
-  
-  timespec now = TraceTool::get_time();
-  for (ulint index = 0, size = locks.size(); index < size; ++index)
-  {
-    lock_t *lock = locks[index];
-    lock->time_so_far = TraceTool::difftime(lock->trx->trx_start_time, now);
-    lock->process_time = estimate(lock->time_so_far, lock->trx->type);
-    
-    if (do_log)
-    {
-      log_file << lock->time_so_far << ",";
-    }
-  }
-  if (do_log)
-  {
-    log_file << endl;
-  
-    for (ulint index = 0, size = locks.size(); index < size; ++index)
-    {
-      log_file << locks[index]->process_time << ",";
-    }
-    log_file << endl;
-  }
-  
-  vector<vector<lock_t *> > perms;
-  permutate(locks, 0, locks.size() - 1, perms);
-  
-  double min_variance = std::numeric_limits<double>::max();
-  int min_var_index = -1;
-  for (ulint index = 0, size = perms.size(); index < size; ++index)
-  {
-    vector<ulint> rolling_sum;
-    cumsum(perms[index], rolling_sum);
-    double variance = var(rolling_sum);
-    if (variance < min_variance)
-    {
-      min_variance = variance;
-      min_var_index = index;
-    }
-  }
-  
-  for (ulint index = 0, size = perms[min_var_index].size();
-       index < size; ++index)
-  {
-    perms[min_var_index][index]->ranking = index;
-    
-    if (do_log)
-    {
-      log_file << perms[min_var_index][index]->time_so_far << ",";
-    }
-  }
-  
-  if (do_log)
-  {
-    log_file << endl;
-    
-    for (ulint index = 0, size = perms[min_var_index].size();
-         index < size; ++index)
-    {
-      log_file << perms[min_var_index][index]->process_time << ",";
-    }
-    log_file << endl;
-  }
-  
-  return perms[min_var_index][0];
-}
-
 
 static
 bool
@@ -469,44 +320,6 @@ lock_get_mode(
   {
     mode.append("R");
   }
-  if (lock->type_mode & LOCK_INSERT_INTENTION)
-  {
-    mode.append("I");
-  }
-  else if (lock->type_mode & LOCK_GAP)
-  {
-    mode.append("G");
-  }
-  else if (lock->type_mode & LOCK_ORDINARY)
-  {
-    mode.append("N");
-  }
-  else if (lock->type_mode & LOCK_REC_NOT_GAP)
-  {
-    mode.append("R");
-  }
-  
-  switch (lock->trx->type)
-  {
-    case NEW_ORDER:
-      mode.append("O");
-      break;
-    case PAYMENT:
-      mode.append("P");
-      break;
-    case ORDER_STATUS:
-      mode.append("S");
-      break;
-    case DELIVERY:
-      mode.append("D");
-      break;
-    case STOCK_LEVEL:
-      mode.append("L");
-      break;
-    default:
-      mode.append("N");
-      break;
-  }
   
   return mode;
 }
@@ -576,8 +389,6 @@ LVM_schedule(
   vector<lock_t *> &granted_locks,  /*!< granted locks */
   vector<lock_t *> &locks_to_grant) /*!< locks to grant */
 {
-  bool do_monitor = rand() % 100 < 1;
-  
   if (waiting_locks.size() == 0)
   {
     return;
@@ -596,27 +407,29 @@ LVM_schedule(
     granted_locks[index]->in_batch = true;
   }
   
-  ofstream &log_file = TraceTool::get_instance()->get_log();
-  
-  vector<lock_t *> all_locks(granted_locks.begin(), granted_locks.end());
-  all_locks.insert(all_locks.end(), waiting_locks.begin(), waiting_locks.end());
   
   timespec now = TraceTool::get_time();
-  for (ulint index = 0, size = all_locks.size(); index < size; ++index)
+  for (ulint index = 0, size = waiting_locks.size(); index < size; ++index)
   {
-    lock_t *lock = all_locks[index];
+    lock_t *lock = waiting_locks[index];
+    trx_t *trx = lock->trx;
     lock->time_so_far = TraceTool::difftime(lock->trx->trx_start_time, now);
-    lock->process_time = estimate(lock->time_so_far, lock->trx->type);
+    ulint wait_so_far = trx->total_wait_time + TraceTool::difftime(lock->wait_start, now);
+    ulint work_so_far = lock->time_so_far - wait_so_far;
+    ulint num_locks = UT_LIST_GET_LEN(lock->trx->lock.trx_locks);
+    ulint estimation = estimate(num_locks, work_so_far, wait_so_far, lock->trx->type);
+    lock->process_time = estimation - lock->time_so_far;
     
-    if (do_monitor &&
-        lock->trx->type != NONE &&
+    if (lock->trx->type != NONE &&
         lock->trx->real_transaction_id != NULL &&
         lock->trx->transaction_id == *(lock->trx->real_transaction_id))
     {
-      TraceTool::get_instance()->add_estimate_record(lock->time_so_far + lock->process_time,
-                                                     lock->trx->transaction_id);
+      TraceTool::get_instance()->add_estimate_record(estimation, lock->trx->transaction_id);
     }
   }
+  
+  vector<lock_t *> all_locks(granted_locks.begin(), granted_locks.end());
+  all_locks.insert(all_locks.end(), waiting_locks.begin(), waiting_locks.end());
   
   vector<int> rankings(waiting_locks.size());
   list<vector<int> > ranking_enumerations;
@@ -663,27 +476,6 @@ LVM_schedule(
     }
   }
   
-  if (do_monitor)
-  {
-    sort(all_locks.begin() + granted_size, all_locks.end(), compare);
-    log_file << granted_locks.size() << "," << waiting_locks.size() << endl;
-    for (ulint index = 0, size = granted_locks.size(); index < size; ++index)
-    {
-      lock_t *lock = granted_locks[index];
-      log_file << "lock_t lock" << index + 1 << "={" << lock->ranking << "," << lock->time_so_far << "," << lock->process_time << ",'"
-      << lock_get_mode(granted_locks[index]) << "'};" << endl;
-    }
-    log_file << endl;
-
-    for (ulint index = 0, size = waiting_locks.size(); index < size; ++index)
-    {
-      lock_t *lock = waiting_locks[index];
-      log_file << "lock_t lock" << index + 1 << "={" << lock->ranking << "," << lock->time_so_far << "," << lock->process_time << ",'"
-      << lock_get_mode(waiting_locks[index]) << "'};" << endl;
-    }
-    log_file << endl;
-  }
-  
   if (granted_locks.size() > 0 &&
       smallest_ranking != 0)
   {
@@ -697,16 +489,4 @@ UNIV_INTERN
 void
 indi_cleanup()
 {
-  free(tpcc_work_wait);
-  free(tpcc_estimated);
-  free(new_order_work_wait);
-  free(new_order_estimated);
-  free(payment_work_wait);
-  free(payment_estimated);
-  free(order_status_work_wait);
-  free(order_status_estimated);
-  free(delivery_work_wait);
-  free(delivery_estimated);
-  free(stock_level_work_wait);
-  free(stock_level_estimated);
 }
