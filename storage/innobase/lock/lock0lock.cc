8ad6c690af0146bc858063038dea1cea41c21d15
sensssz <hjmsens@gmail.com>
2015-06-11 00:03:11 -0400

    Original

--- storage/innobase/lock/lock0lock.cc
+++ storage/innobase/lock/lock0lock.cc
@@ -2545,12 +2545,12 @@ lock_grant(
   ulint wait_time = TraceTool::difftime(lock->wait_start, now);
   trx->total_wait_time += wait_time;
   
-  if (lock->process_time > 0 &&
-      trx->is_user_trx)
-  {
-    ulint time_so_far = TraceTool::difftime(trx->trx_start_time, now);
-    TraceTool::get_instance()->add_estimate_record(time_so_far, lock->process_time, trx->transaction_id);
-  }
+//  if (lock->process_time > 0 &&
+//      trx->is_user_trx)
+//  {
+//    ulint time_so_far = TraceTool::difftime(trx->trx_start_time, now);
+//    TraceTool::get_instance()->add_estimate_record(time_so_far, lock->process_time, trx->transaction_id);
+//  }
 
   lock_reset_lock_and_trx_wait(lock);
 
@@ -2873,7 +2873,6 @@ lock_rec_dequeue_from_page(
 	MONITOR_INC(MONITOR_RECLOCK_REMOVED);
 	MONITOR_DEC(MONITOR_NUM_RECLOCK);
   
-  /*
   for (lock = lock_rec_get_first_on_page_addr(space, page_no);
        lock != NULL;
        lock = lock_rec_get_next_on_page(lock))
@@ -2884,47 +2883,46 @@ lock_rec_dequeue_from_page(
       lock_grant(lock);
     }
   }
-   */
   
-  /* Find the first lock on this paper. If we cannot find one, we can simply stop. */
-  lock_t *first_lock_on_page = lock_rec_get_first_on_page_addr(space, page_no);
-  if (first_lock_on_page == NULL)
-  {
-      return;
-  }
-  
-  /* A lock object can represent multiple locks on the same page. We look at each one of them. */
-  for (ulint heap_no = 0, n_bits = lock_rec_get_n_bits(in_lock);
-       heap_no < n_bits; ++heap_no)
-  {
-    /* Not a lock on this record. */
-    if (!lock_rec_get_nth_bit(in_lock, heap_no))
-    {
-      continue;
-    }
-    
-    
-    vector<lock_t *> locks_to_grant;
-    lock_next_to_grant(space, page_no, heap_no, locks_to_grant);
-    locks_grant(locks_to_grant, space, page_no, heap_no, in_lock->trx);
-    
-    /* Find other locks that can also be granted. */
-    lock = lock_rec_get_first_on_page_addr(space, page_no);
-    if (!lock_rec_get_nth_bit(lock, heap_no))
-    {
-      lock = lock_rec_get_next(heap_no, lock);
-    }
-    for (; lock != NULL; lock = lock_rec_get_next(heap_no, lock))
-    {
-      if (lock_get_wait(lock))
-      {
-        if (!lock_rec_has_to_wait_in_queue(lock))
-        {
-          lock_grant(lock);
-        }
-      }
-    }
-  }
+//  /* Find the first lock on this paper. If we cannot find one, we can simply stop. */
+//  lock_t *first_lock_on_page = lock_rec_get_first_on_page_addr(space, page_no);
+//  if (first_lock_on_page == NULL)
+//  {
+//      return;
+//  }
+//  
+//  /* A lock object can represent multiple locks on the same page. We look at each one of them. */
+//  for (ulint heap_no = 0, n_bits = lock_rec_get_n_bits(in_lock);
+//       heap_no < n_bits; ++heap_no)
+//  {
+//    /* Not a lock on this record. */
+//    if (!lock_rec_get_nth_bit(in_lock, heap_no))
+//    {
+//      continue;
+//    }
+//    
+//    
+//    vector<lock_t *> locks_to_grant;
+//    lock_next_to_grant(space, page_no, heap_no, locks_to_grant);
+//    locks_grant(locks_to_grant, space, page_no, heap_no, in_lock->trx);
+//    
+//    /* Find other locks that can also be granted. */
+//    lock = lock_rec_get_first_on_page_addr(space, page_no);
+//    if (!lock_rec_get_nth_bit(lock, heap_no))
+//    {
+//      lock = lock_rec_get_next(heap_no, lock);
+//    }
+//    for (; lock != NULL; lock = lock_rec_get_next(heap_no, lock))
+//    {
+//      if (lock_get_wait(lock))
+//      {
+//        if (!lock_rec_has_to_wait_in_queue(lock))
+//        {
+//          lock_grant(lock);
+//        }
+//      }
+//    }
+//  }
 }
 
 /*************************************************************//**
