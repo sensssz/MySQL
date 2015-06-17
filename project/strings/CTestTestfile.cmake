feecaeba36005e04edeeb111404fe4ec1d055d23
sensssz <hjmsens@gmail.com>
2015-06-17 14:14:13 -0400

    Remove project folder

--- project/strings/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sens/Developer/mysql-5.6.23/strings
-# Build directory: /Users/sens/Developer/mysql-5.6.23/project/strings
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- project/unittest/mytap/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sens/Developer/mysql-5.6.23/unittest/mytap
-# Build directory: /Users/sens/Developer/mysql-5.6.23/project/unittest/mytap
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
