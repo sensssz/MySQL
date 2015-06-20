e0660e3c12538ba5f42f179d33aa47e8747570c1
sensssz <hjmsens@gmail.com>
2015-06-20 09:58:51 -0400

    min_var

--- storage/innobase/lock/lock0var.cc
+++ storage/innobase/lock/lock0var.cc
@@ -555,15 +555,15 @@ LVM_schedule(
     ulint num_locks = UT_LIST_GET_LEN(trx->lock.trx_locks);
     work_wait parameters = TraceTool::get_instance()->parameters_necessary(work_so_far, wait_so_far, num_locks,
                                                                  wait_locks.size(), trx->transaction_id);
-    lock->process_time = exp(estimate(parameters, trx->type));
+    lock->process_time = estimate(parameters, trx->type);
     
-    if ((rand() % 100 < 20 ||
-         trx->type == ORDER_STATUS) &&
-        trx->is_user_trx)
-    {
-      lock->time_at_grant = TraceTool::get_instance()->add_work_wait(work_so_far, wait_so_far, num_locks,
-                                                                     wait_locks.size(), lock->process_time, trx->transaction_id);
-    }
+//    if ((rand() % 100 < 20 ||
+//         trx->type == ORDER_STATUS) &&
+//        trx->is_user_trx)
+//    {
+//      lock->time_at_grant = TraceTool::get_instance()->add_work_wait(work_so_far, wait_so_far, num_locks,
+//                                                                     wait_locks.size(), lock->process_time, trx->transaction_id);
+//    }
   }
   estimate_mutex_exit();
   
